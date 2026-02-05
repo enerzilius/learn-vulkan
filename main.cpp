68ea1aaf4a57df1a845c61b759aac9b66eb5ee67
enerzilius <efblouback@gmail.com>
2026-02-05 15:10:11 -0300

    feat: adicionando os eventos do glfw

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
 
-#define WINDOW_HEIGHT 1920
-#define WINDOW_WIDTH 1080
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
 
 static void error_callback(int error, const char* description)
 {
@@ -13,32 +14,47 @@ static void error_callback(int error, const char* description)
 static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
-
-GLFWwindow* window = NULL;
 
 int main(int argc, char** argv)
 {
+  GLFWwindow* window;
+
+  glfwSetErrorCallback(error_callback);
+
   if(!glfwInit()){
     std::cout<<"ERROR INITATING GLFW!\n\n";
-    return 1;
+    exit(EXIT_FAILURE);
   }
 
   if(!glfwVulkanSupported()) {
     std::cout<<"VULKAN NOT SUPPORTED!\n\n";
-    return 1;
+    exit(EXIT_FAILURE);
   }
-
-  window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "VULKAN", NULL, NULL);
+  
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  glfwWindowHint(GLFW_FOCUSED, GLFW_TRUE);
+  window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "VULKAN", NULL, NULL); 
 
   if(!window) {
     glfwTerminate();
     exit(EXIT_FAILURE);
     return 1;
   }
- 
+
+  glfwSetKeyCallback(window, key_callback);
+  glfwShowWindow(window);
+
   std::cout<<"PROJETO COM VULKAN xD\n";
 
-  return 0;
+  while(!glfwWindowShouldClose(window)) {  
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+ 
+  exit(EXIT_SUCCESS); 
 }
